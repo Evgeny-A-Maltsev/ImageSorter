98b72b032ede1258b024fad86d02ba5028244e48
Evgeny A. Maltsev <yevmal@gmail.com>
2024-05-04 19:48:10 +0300

    Small changes in the code

---
+++ main.py
@@ -0,0 +1,15 @@
+import click
+
+@click.command()
+@click.option('-s', '--source', help='Photo source directory')
+@click.option('-d', '--destination', help='Photo destination directory')
+@click.option('-r', '--recursive', is_flag=True, default=True, help='Recursively photo search')
+@click.option('-m', '--move', is_flag=True, default=True, help='Move sorted photos')
+def image_soft(source, destination, recursive, move):
+    click.echo(f"Photo source directory = {source}")
+    click.echo(f"Photo destination directory = {destination}")
+    click.echo(f"Recursively photo search = {recursive}")
+    click.echo(f"Move sorted photos = {move}")
+
+if __name__ == '__main__':
+    image_soft()
