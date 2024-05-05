889c4f66f4cdd3ac4da30dd27d2905e2ed3d1ddc
Evgeny A. Maltsev <yevmal@gmail.com>
2024-05-05 23:07:59 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import click
 
+
 @click.command()
 @click.option('-s', '--source', help='Photo source directory')
 @click.option('-d', '--destination', help='Photo destination directory')
 @click.option('-r', '--recursive', is_flag=True, default=True, help='Recursively photo search')
 @click.option('-m', '--move', is_flag=True, default=True, help='Move sorted photos')
-def image_soft(source, destination, recursive, move):
+def image_sort(source, destination, recursive, move):
     click.echo(f"Photo source directory = {source}")
     click.echo(f"Photo destination directory = {destination}")
     click.echo(f"Recursively photo search = {recursive}")
     click.echo(f"Move sorted photos = {move}")
 
+
 if __name__ == '__main__':
-    image_soft()
+    image_sort()
