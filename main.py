b3824fa106fed5fc5b2db79216ad9c6d2bbf4318
Evgeny A. Maltsev <yevmal@gmail.com>
2024-05-10 11:10:02 +0300

    Refactoring variables

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import click
 
 
 @click.command("cli", context_settings={'show_default': True})
-@click.argument('source', required=True)
-@click.argument('targetr', required=True)
+@click.argument('source_directory', required=True)
+@click.argument('destination_directory', required=True)
 @click.option('-r', '--recursive', is_flag=True, default=False, help='Recursively photo search')
 @click.option('-m', '--move', is_flag=True, default=False, help='Move sorted photos')
-def image_sort(source, target, recursive, move):
-    click.echo(f"Photo source directory = {source}")
-    click.echo(f"Photo destination directory = {target}")
+def image_sort(source_directory, destination_directory, recursive, move):
+    click.echo(f"Photo source directory = {source_directory}")
+    click.echo(f"Photo destination directory = {destination_directory}")
     click.echo(f"Recursively photo search = {recursive}")
     click.echo(f"Move sorted photos = {move}")
 
